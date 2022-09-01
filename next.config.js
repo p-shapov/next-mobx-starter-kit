58c0c0af1dbd0d5912f2c7cc2b8e4e933425b23a
Pavel Shapov <pavel.shapov@metalamp.io>
2022-09-02 01:18:39 +0700

    Setup env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    additionalData: '@import "src/shared/styles/global.scss";',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
