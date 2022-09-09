cc8679d6242fbf7018ec7e6fc793595954fe8b57
Pavel Shapov <pavel.shapov@metalamp.io>
2022-09-09 16:57:59 +0700

    Update

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import 'the-new-css-reset';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
