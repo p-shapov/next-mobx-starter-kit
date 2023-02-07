d20e77a5cfbc0b607824ec0c3eeb69419c293956
Pavel Shapov <pavel.shapov@metalamp.io>
2023-02-07 16:30:21 +0600

    test: ssg

--- pages/test/[id].ts
+++ pages/test/[id].ts
@@ -1,4 +1,6 @@
-import { GetServerSideProps } from 'next';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable max-len */
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
 
 import type { Props } from 'module/Home';
 
@@ -6,10 +8,15 @@ import { api } from 'service/API/core';
 
 export { Home as default } from 'module/Home';
 
-export const getServerSideProps: GetServerSideProps<Props, { id: 'Soon' | 'Started' | 'Finished' }> = async (
-  req,
-) => {
-  const phase = req.params?.id;
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: 'Soon' } }, { params: { id: 'Started' } }, { params: { id: 'Finished' } }],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, { id: 'Soon' | 'Started' | 'Finished' }> = async (ctx) => {
+  const phase = ctx.params?.id;
 
   if (!phase)
     return {
@@ -20,10 +27,28 @@ export const getServerSideProps: GetServerSideProps<Props, { id: 'Soon' | 'Start
   const supply = (await api.get<number>(`/supply/${phase}/`)).data;
 
   return {
-    props: {
-      price,
-      phase,
-      supply,
-    },
+    props: { price, supply, phase },
   };
 };
+
+// export const getServerSideProps: GetServerSideProps<Props, { id: 'Soon' | 'Started' | 'Finished' }> = async (
+//   req,
+// ) => {
+//   const phase = req.params?.id;
+
+//   if (!phase)
+//     return {
+//       notFound: true,
+//     };
+
+//   const price = (await api.get<number>('/price/')).data;
+//   const supply = (await api.get<number>(`/supply/${phase}/`)).data;
+
+//   return {
+//     props: {
+//       price,
+//       phase,
+//       supply,
+//     },
+//   };
+// };
