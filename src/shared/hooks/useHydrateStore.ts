34b1a40c7e10c8196ddca7297b6fefd33a870b0e
Pavel Shapov <pavel.shapov@metalamp.io>
2023-02-07 15:33:58 +0600

    fix: useHydrateStore types

--- src/shared/hooks/useHydrateStore.ts
+++ src/shared/hooks/useHydrateStore.ts
@@ -1,9 +1,6 @@
 import { useEffect, useMemo } from 'react';
 
-export const useHydrateStore = <T, S extends { hydrate(data: T): void; dehydrate(): void }>(
-  data: T,
-  store: S,
-) => {
+export const useHydrateStore = <T>(data: T, store: { hydrate(data: T): void; dehydrate(): void }) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useMemo(() => store.hydrate(data), []);
 
